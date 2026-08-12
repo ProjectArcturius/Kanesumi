1a3a879488a0ad84993baebb3186c818b7a81e7b
TakahashiRinta <1586182829@qq.com>
2026-08-12 10:54:12 +0800

    chore(controls): lib.rs 注册全部新控件模块 -- Repeater/ScrollView/TextBox/PasswordField/CheckBox/NumberBox/AutoSuggestBox/CommandBarFlyout/级联类型

--- kanesumi-controls/src/lib.rs
+++ kanesumi-controls/src/lib.rs
@@ -6,9 +6,12 @@
 // 状态驱动渲染：控件持有状态，`render(theme, engine, rect, scene)` 把当前状态解析为 Scene 命令。
 
 pub mod animated_icon;
+pub mod auto_suggest_box;
 pub mod breadcrumb_bar;
 pub mod button;
+pub mod check_box;
 pub mod color_picker;
+pub mod command_bar_flyout;
 pub mod decl;
 pub mod dialog;
 pub mod drop_down_button;
@@ -21,15 +24,19 @@ pub mod list;
 pub mod menu_bar;
 pub mod metro_tile;
 pub mod navigation_view;
+pub mod number_box;
 pub mod pager_control;
 pub mod parallax_view;
+pub mod password_box;
 pub mod person_picture;
 pub mod pips_pager;
 pub mod popup;
 pub mod progress;
 pub mod radio_buttons;
 pub mod rating_control;
+pub mod repeater;
 pub mod retained;
+pub mod scroll_view;
 pub mod selector_flyout;
 pub mod split_button;
 pub mod state;
@@ -40,20 +47,28 @@ pub mod tab_row;
 pub mod tab_view;
 pub mod teaching_tip;
 pub mod text;
+pub mod text_box;
+pub mod text_field;
 pub mod title_bar;
 pub mod tree_view;
 pub mod two_pane_view;
 
 pub use animated_icon::{IconDirection, MetroAnimatedIcon};
+pub use auto_suggest_box::{AutoSuggestAction, MetroAutoSuggestBox};
 pub use breadcrumb_bar::{BreadcrumbClick, MetroBreadcrumbBar};
 pub use button::{ButtonKind, MetroButton};
+pub use check_box::{CheckState, MetroCheckBox};
 pub use color_picker::{ALL_CHANNELS, ColorChannel, MetroColorPicker};
+pub use command_bar_flyout::{
+    COMMANDBAR_BORDER, COMMANDBAR_BUTTON_SIZE, COMMANDBAR_ICON_SIZE, TEXT_COMMANDS, CommandBarAction,
+    CommandButton, MetroCommandBarFlyout,
+};
 pub use decl::{
     Decl, DeclAction, DeclChange, DeclHit, DeclPath, collect_hits, diff_decl, render_decl,
 };
 pub use dialog::{DialogButton, DialogButtons, DialogDefaultButton, DialogState, MetroDialog};
 pub use drop_down_button::MetroDropDownButton;
-pub use dropdown_menu::{MenuItem, MetroDropdownMenu};
+pub use dropdown_menu::{MenuItem, MenuPath, MetroDropdownMenu, SubmenuState};
 pub use expander::{ExpandDirection, MetroExpander};
 pub use icon_button::MetroIconButton;
 pub use info_badge::{InfoBadgeKind, MetroInfoBadge};
@@ -64,8 +79,10 @@ pub use metro_tile::{MetroTile, TileLive, TileSize};
 pub use navigation_view::{
     MetroNavigationView, NavigationAction, NavigationPaneMode, NavigationViewItem,
 };
+pub use number_box::{MetroNumberBox, SpinButton, SpinPlacement};
 pub use pager_control::{MetroPagerControl, PagerAction, PagerHover, PagerItem};
 pub use parallax_view::MetroParallaxView;
+pub use password_box::{MetroPasswordBox, PASSWORD_MASK_CHAR};
 pub use person_picture::{MetroPersonPicture, initials_from_display_name};
 pub use pips_pager::{MetroPipsPager, PipsAction, PipsOrientation};
 pub use popup::{
@@ -75,7 +92,12 @@ pub use popup::{
 pub use progress::{MetroProgressBar, MetroProgressRing, ProgressMode};
 pub use radio_buttons::MetroRadioButtons;
 pub use rating_control::MetroRatingControl;
+pub use repeater::{MetroRepeater, RepeaterLayout, RepeaterOrientation};
 pub use retained::RetainedScene;
+pub use scroll_view::{
+    SCROLLBAR_MIN_THUMB, SCROLLBAR_THICKNESS, SCROLL_WHEEL_STEP, MetroScrollView, ScrollBarVisibility,
+    ScrollMode,
+};
 pub use selector_flyout::MetroSelectorFlyout;
 pub use split_button::{MetroSplitButton, SplitButtonClick, SplitButtonPart};
 pub use state::{ButtonState, ControlState};
@@ -86,6 +108,8 @@ pub use tab_row::{MetroTab, MetroTabRow};
 pub use tab_view::{MetroTabView, TabHover, TabViewAction};
 pub use teaching_tip::{MetroTeachingTip, TeachingTipClick, TeachingTipPlacement, TeachingTipSide};
 pub use text::MetroText;
+pub use text_box::MetroTextBox;
+pub use text_field::{TextInputKey, TextField};
 pub use title_bar::{MetroTitleBar, TitleBarClick};
 pub use tree_view::{MetroTreeView, TreeAction, TreeRow, TreeViewNode};
 pub use two_pane_view::{
